201acf79b53f448f5839acefb127c422d8f582dc
HanRewan <hristopsis@gmail.com>
2023-07-17 22:15:16 +0300

    first load

---
+++ SimpleForum/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for SimpleForum project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SimpleForum.settings')
+
+application = get_asgi_application()
